d03c81493399478d8092d24220195b27dc88d6b4
sammieulric <olatunjiolaide30@gmail.com>
2025-01-29 14:33:08 +0100

    get user update

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,7 +37,7 @@ const createUser = async (req, res) => {
 // Get Users
 const getUsers = async (req, res) => {
     try {
-        const users = await User.findAll({ attributes: ['id', 'username', 'email', 'role', 'created_at'] });
+        const users = await User.findAll({ attributes: ['id', 'username', 'email', 'role'] });
         res.json(users);
     } catch (error) {
         res.status(500).json({ message: error.message });
