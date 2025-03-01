16058e09f73d2c14e552a27b84de9786f5075a6e
sammieulric <olatunjiolaide30@gmail.com>
2025-03-01 10:38:26 +0100

    user management api update - user validation2

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 
 // Generate JWT Token
 const generateToken = (id, role) => {
-    return jwt.sign({ id, role }, process.env.JWT_SECRET, { expiresIn: '30d' });
+    return jwt.sign({ id, role }, process.env.JWT_SECRET, { expiresIn: '2m' });
 };
 
 // Create User (Registration)
